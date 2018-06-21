2f9e63c9ba144795cfe01b58f69a7b7555ca80f1
Christine Xiang <yutingcxiang@gmail.com>
2018-06-21 00:32:54 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,8 +13,8 @@ function frameIt() {
 
 function pressIt() {
   $('input').on('keydown', function() {
-    if (key.which == 13){
-    alert('enter was pressed');
+    if (key.which == 71){
+    alert('G was pressed');
   })
 }
 
