530c8956421e23a79e97b6ff99ef57c67b97aa4a
Christine Xiang <yutingcxiang@gmail.com>
2018-06-21 00:35:03 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,6 +19,12 @@ function pressIt() {
   });
 }
 
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
